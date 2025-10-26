17954b92a7d69cf92a90ec973520c35aeaca5207
PawatW <pawat.w@ku.th>
2025-10-26 15:17:35 +0700

    add cloud service

--- demo1/demo/src/main/java/com/inv/repo/RequestRepository.java
+++ demo1/demo/src/main/java/com/inv/repo/RequestRepository.java
@@ -52,7 +52,7 @@ public class RequestRepository {
 
     public List<Request> findByOrderId(String orderId) {
         String sql = "SELECT request_id, request_date, status, order_id, customer_id, staff_id, description, approved_by, approved_date " +
-                "FROM request WHERE order_id = ?"; // <-- ใช้ ? เพื่อความปลอดภัย
+                "FROM request WHERE order_id = ?"; 
 
         // ส่ง orderId เข้าไปเป็น argument ตัวที่ 3
         return jdbcTemplate.query(sql, this::mapRow, orderId);
